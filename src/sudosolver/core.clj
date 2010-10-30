e007d387d410905ed2e2dad3adad029dcab8b927
Stephane Tavera <stephane@inagua.ch>
2010-10-30 16:33:16 +0200

    first tests

--- src/sudosolver/core.clj
+++ src/sudosolver/core.clj
@@ -1,1 +1,18 @@
-(ns sudosolver.core)
+(ns sudosolver.core
+    (:use clojure.set))
+
+(defn no-duplicate? [coll]
+    (= (count coll) (count (distinct coll))))
+
+(defn valid? [cells]
+    (and
+        (= 9 (count cells))
+        (no-duplicate? (remove #(= 0 %) cells))
+        (every? #(<= 0 % 9) cells)))
+
+(defn candidates
+    ([coll] (difference #{1 2 3 4 5 6 7 8 9} (set coll)))
+    ([col1 col2 col3]
+        (intersection   (candidates col1) 
+                        (candidates col2) 
+                        (candidates col3))))
--- test/sudosolver/test/core.clj
+++ test/sudosolver/test/core.clj
@@ -2,5 +2,26 @@
   (:use [sudosolver.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest valid?-accepts-correct-sets
+    (is (valid? [0 0 0 0 0 0 0 0 0]))    
+    (is (valid? [0 0 0 4 5 6 7 8 9]))
+    (is (valid? [1 2 3 4 5 6 7 8 9])))
+    
+(deftest valid?-check-length 
+    (is (not (valid? [1 2 3])))
+    (is (not (valid? [1 2 3 4 5 6 7 8 9 0]))))
+     
+(deftest valid?-checks-each-element-between-1-and-9
+    (is (not (valid? [1 10 3 4 5 6 7 8 9]))))
+    
+(deftest valid?-checks-no-duplicate
+    (is (not (valid? [1 1 3 4 5 6 7 8 9]))))
+    
+(deftest check-candidate
+    (is (= #{1}     (candidates [2 3 4 5 6 7 8 9])))
+    (is (= #{1 2 3} (candidates [0 0 4 5 6 7 8 9])))
+    (is (= #{2}     (candidates [0 0 4 5 6 7 8 9] 
+                                [1 0 4 0 0 0 0 0]
+                                [3 0 4 0 0 0 0 0]))))
+
+ 
